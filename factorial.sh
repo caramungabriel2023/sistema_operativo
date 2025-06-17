70fe504027f9f3c37097882dd8e3210a6122b8a5
caramungabriel2023 <gabriel.caramun@alumnos.ispi4038.edu.ar>
2025-06-17 19:22:21 -0300

    Avance de programa (en revicion)

--- factorial.sh
+++ factorial.sh
@@ -1,3 +1,17 @@
 #3. Haz un script con una función que calcule el factorial de un número entero positivo:
 #En éste caso se multiplica el número por todos los enteros positivos menores que él, hasta llegar a 1.
 #!/bin/bash
+
+echo "Introduce un número entero positivo:"
+read numero
+if (($numero < 0)); then
+  echo "El número debe ser entero positivo."
+  exit 1
+else
+    factorial=1
+    for (( i=1; i<=numero; i++ )); do
+        factorial=$((factorial * i))
+    done
+    echo "El factorial de $numero es $factorial"
+echo "el numero esta bien"
+fi
