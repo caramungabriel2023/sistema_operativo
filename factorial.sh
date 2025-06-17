536715478d7cae77832c0d009087bb19b9d641f8
caramungabriel2023 <gabriel.caramun@alumnos.ispi4038.edu.ar>
2025-06-17 18:40:39 -0300

    Ejercicio en proceso: Factorial

---
+++ factorial.sh
@@ -0,0 +1,3 @@
+#3. Haz un script con una función que calcule el factorial de un número entero positivo:
+#En éste caso se multiplica el número por todos los enteros positivos menores que él, hasta llegar a 1.
+#!/bin/bash
